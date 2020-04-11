13a2c001c03bb7805d8c7b3a4f385e347ef0845c
ericdasse28 <christ.gououi@gmail.com>
2020-04-12 00:58:25 +0200

    Définition de la classe Joueur

--- src/chifoumi/Joueur.java
+++ src/chifoumi/Joueur.java
@@ -1,5 +1,35 @@
 package chifoumi;
 
 public class Joueur {
+	
+	private String nom; //TODO: Init in constructor
+	private int score = 0;
+	private IHMSaisie saisie; //TODO: Init in constructor
+	
+	public Joueur(String nom) {
+		this.nom = nom;
+		saisie = new IHMSaisie(nom);
+		
+	}
+	
+	public void crediter() {
+		this.score++;
+	}
+	
+	public Choix choisir() {
+		return saisie.proposerChoix();
+		
+	}
+	
+	public int getScore() {
+		return this.score;
+		
+	}
+	
+	public void ecrireScore() {
+		System.out.println("Le score de " + this.nom + " est " + this.score);
+		
+	}
+	
 
 }
